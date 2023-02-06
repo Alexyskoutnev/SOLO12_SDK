3324fb8b454b8fdba052f1393ee5db2f9943a7ce
Alexy <alexyskoutnev@gmail.com>
2023-02-05 23:52:04 -0600

    more edits

--- scripts/run.cpp
+++ scripts/run.cpp
@@ -10,6 +10,27 @@
 #define N_DRIVER_CNT 6
 #define TIMEOUT 5
 
+#define DELIMITER ','
+
+template <typename T>
+std::vector<std::vector<T>> csv_reader(std::string file_name){
+    std::string line;
+    std::vector<std::vector<T>> rows {};
+    std::ifstream file(file_name, std::ifstream::in);
+    while (getline(file, line)){
+        char tmpChar;
+        std::vector<T> tmpVec;
+        std::stringstream ss(line);
+        std::string tmpstr;
+        rows.push_back(tmpVec);
+        while (getline(ss, tmpstr, DELIMITER)){
+            std::cout << "tmpstr " << tmpstr << std::endl;
+            rows.back().push_back(std::stod(tmpstr));
+        }
+    }
+    return rows;
+}
+
 int main(int argc, char** argv){
     std::cout << "Main Script " << std::endl;
     std::string file_name = NULL;
