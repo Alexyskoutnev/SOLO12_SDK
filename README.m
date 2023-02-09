917c33b2f6e4c769c00858e0d3d38a089396a0cf
Alexy Skoutnev <59209783+Alexyskoutnev@users.noreply.github.com>
2023-02-08 22:33:19 -0600

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,3 @@
-Interface wrapper for the SOLO12 quadrupted
+#Interface wrapper for the SOLO12 quadrupted
+##This library helps with communicating with the low level controller onboard SOLO12
+##Setup
